5c5e77dcca70f32063bd24112ed0aa48365f2020
3210snoop3210 <58981273+3210snoop3210@users.noreply.github.com>
2025-03-14 18:37:42 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';  // 'react-dom/client' is the new import path in React 18
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.createRoot(div).render(<App />);
 });
